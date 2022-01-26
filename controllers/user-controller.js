bfc454ea48e3f7d080e02a254e5563e9a7a3bf2b
Syke2good <akashbhandari930@gmail.com>
2022-01-25 21:18:04 -0800

    fixed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -85,7 +85,7 @@ module.exports = {
     console.log(req.body);
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $addToSet: { friends: req.body } },
+      { $addToSet: { friends: req.params.friendId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
@@ -101,7 +101,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friend: { friendId: req.params.friendId } } },
+      { $pull: { friends: req.params.friendId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
